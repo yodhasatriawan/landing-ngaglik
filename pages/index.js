b810934b5e27b9c2237a70d6421bf9000bdb7d14
yodhasatriawan <yodha413@gmail.com>
2023-03-27 14:54:12 +0700

    card portfoliow section

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import YoutubeIcon from '@/components/svgs/youtube'
 import TaskIcon from '@/components/svgs/task'
 import BookIcon from '@/components/svgs/book'
 import Radar from '@/components/svgs/radar'
+import Card from '@/components/pages/Card'
 
 const menus = [
   { href: '/', text: 'home' },
@@ -33,10 +34,33 @@ const icons = [
   },
 ]
 
+import React from 'react'
+
+const portfolios = [
+  {
+    name: 'Bookies',
+    image:
+      'https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    description: 'read digital book application',
+  },
+  {
+    name: 'BWallet',
+    image:
+      'https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    description: 'digital wallet application',
+  },
+  {
+    name: 'GOJEK',
+    image:
+      'https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=1200',
+    description: 'digital wallet application',
+  },
+]
+
 export default function Home() {
   return (
     <>
-      <header className="fixed w-full bg-gradient-to-b from-white">
+      <header className="z-[99] fixed w-full bg-gradient-to-b from-white">
         <div className="py-3 md:py-4 px-4 md:px-12 xl:px-24 mx-auto flex flex-row justify-between items-center ">
           <div className="flex items-center space-x-6">
             <a
@@ -109,7 +133,7 @@ export default function Home() {
                 programing languages and multi platforms.
               </h4>
               {/* Buttons */}
-              <div className="flex space-x-4">
+              <div className="z-[-99] flex space-x-4">
                 <button className="py-2 px-6 text-sm leading-[160%] bg-brand rounded-full text-white">
                   Start learn
                 </button>
@@ -121,6 +145,41 @@ export default function Home() {
                   ))}
                 </div>
               </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Portfolio Cards */}
+        <section>
+          {/* <Card /> */}
+          <div className=" py-14 ">
+            <div className="flex flex-col justify-center space-y-3">
+              {portfolios.map((portofolio, index) => (
+                <div
+                  key={index}
+                  className="bg-white px-3 py-3 border-[1.6px] border-dotted border-[#E5E5E5] rounded-xl"
+                >
+                  <div className="rounded-xl">
+                    <img
+                      src={portofolio.image}
+                      className="w-80 h-60 object-cover rounded-xl"
+                    />
+                  </div>
+                  <div className="pt-4 pb-2 px-2">
+                    <h3 className="font-medium text-[18px] text-text">
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {portofolio.name}
+                    </h3>
+                    <h4 className="text-[14px] text-text">
+                      <span aria-hidden="true" className="absolute inset-0" />
+                      {portofolio.description}
+                    </h4>
+                  </div>
+                </div>
+              ))}
+              <button className="inline-block py-3 px-6 text-sm leading-[160%] border border-brand rounded-full">
+                Load More
+              </button>
             </div>
           </div>
         </section>
